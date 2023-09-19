397b74b034b37a0113e97c485595dbafb47b3f6a
aeither <36173828+aeither@users.noreply.github.com>
2023-09-19 19:20:12 +0300

    ingredients select

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,53 +1,122 @@
-import lighthouse from "@lighthouse-web3/sdk";
-import { DealParameters } from "@lighthouse-web3/sdk/dist/types";
-import React from "react";
+import React, { useState } from "react";
 
-export const Dashboard = () => {
-  const progressCallback = (progressData: {
-    total: number;
-    uploaded: number;
-  }) => {
-    let percentageDone = (
-      100 -
-      progressData?.total / progressData?.uploaded
-    ).toFixed(2);
-    console.log(percentageDone);
+interface Item {
+  id: number;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export function Dashboard() {
+  const initialItems: Item[] = [
+    { id: 1, name: "Item 1", price: 10, quantity: 0 },
+    { id: 2, name: "Item 2", price: 15, quantity: 0 },
+    { id: 3, name: "Item 3", price: 20, quantity: 0 },
+  ];
+
+  const [items, setItems] = useState(initialItems);
+  const [cart, setCart] = useState<Item[]>([]);
+  const [searchQuery, setSearchQuery] = useState<string>("");
+  const [cartText, setCartText] = useState<string>("");
+
+  const addToSelected = (item: Item) => {
+    const updatedCart = [...cart];
+    const existingItem = updatedCart.find(
+      (cartItem) => cartItem.id === item.id
+    );
+
+    if (existingItem) {
+      existingItem.quantity++;
+    } else {
+      updatedCart.push({ ...item, quantity: 1 });
+    }
+
+    setCart(updatedCart);
   };
 
-  const uploadFileOriginal = async (file: any) => {
-    const apiKey = import.meta.env.VITE_LIGHTHOUSE_API_KEY; //generate from https://files.lighthouse.storage/ or cli (lighthouse-web3 api-key --new)
-    console.log("🚀 ~ file: index.tsx:19 ~ uploadFileOriginal ~ apiKey:", apiKey)
-    if (!apiKey) return;
+  const reduceQuantity = (item: Item) => {
+    const updatedCart = [...cart];
+    const existingItem = updatedCart.find(
+      (cartItem) => cartItem.id === item.id
+    );
 
-    const dealParam = {
-      network: "calibration",
-    };
-    const output = await lighthouse.upload(
-      file,
-      apiKey as string,
-      false,
-      dealParam as DealParameters,
-      progressCallback
-    );
-    console.log("File Status:", output);
-    /*
-      output:
-        data: {
-          Name: "filename.txt",
-          Size: 88000,
-          Hash: "QmWNmn2gr4ZihNPqaC5oTeePsHvFtkWNpjY3cD6Fd5am1w"
-        }
-      Note: Hash in response is CID.
-    */
-    console.log(
-      "Visit at https://gateway.lighthouse.storage/ipfs/" + output.data.Hash
-    );
+    if (existingItem && existingItem.quantity > 1) {
+      existingItem.quantity--;
+    } else {
+      const itemIndex = updatedCart.findIndex(
+        (cartItem) => cartItem.id === item.id
+      );
+      if (itemIndex !== -1) {
+        updatedCart.splice(itemIndex, 1); // Remove the item from the cart if quantity is 1 or less
+      }
+    }
+
+    setCart(updatedCart);
   };
+
+  const increaseQuantity = (item: Item) => {
+    const updatedCart = [...cart];
+    const existingItem = updatedCart.find(
+      (cartItem) => cartItem.id === item.id
+    );
+
+    if (existingItem) {
+      existingItem.quantity++;
+    }
+
+    setCart(updatedCart);
+  };
+
+  const removeItem = (item: Item) => {
+    const updatedCart = cart.filter((cartItem) => cartItem.id !== item.id);
+    setCart(updatedCart);
+  };
+
+  const handleExportCart = () => {
+    const cartItemsText = cart
+      .map(
+        (cartItem) =>
+          `${cartItem.name} - $${cartItem.price} x ${cartItem.quantity}`
+      )
+      .join("\n");
+    setCartText(cartItemsText);
+  };
+
+  const filteredItems = items.filter((item) =>
+    item.name.toLowerCase().includes(searchQuery.toLowerCase())
+  );
 
   return (
-    <>
-      <div>Profile</div>
-      <input onChange={(e) => uploadFileOriginal(e.target.files)} type="file" />
-    </>
+    <div className="App">
+      <h1>Items</h1>
+      <input
+        type="text"
+        placeholder="Search items..."
+        value={searchQuery}
+        onChange={(e) => setSearchQuery(e.target.value)}
+      />
+      <ul>
+        {filteredItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price}
+            <button onClick={() => addToSelected(item)}>Add to Cart</button>
+          </li>
+        ))}
+      </ul>
+      <h2>Selected Ingredients</h2>
+      <ul>
+        {cart.map((cartItem) => (
+          <li key={cartItem.id}>
+            {cartItem.name} - ${cartItem.price} x {cartItem.quantity}
+            <button onClick={() => reduceQuantity(cartItem)}>-</button>
+            <button onClick={() => increaseQuantity(cartItem)}>+</button>
+            <button onClick={() => removeItem(cartItem)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={handleExportCart}>Export Ingredients</button>
+      <h2>Ingredients</h2>
+      <textarea rows={5} cols={30} value={cartText} readOnly />
+    </div>
   );
-};
+}

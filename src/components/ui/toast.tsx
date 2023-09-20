c82d8a3a4be60278b1b1473150770f52c39db6b5
aeither <36173828+aeither@users.noreply.github.com>
2023-09-20 23:42:40 +0300

    rm type in toast

--- src/components/ui/toast.tsx
+++ src/components/ui/toast.tsx
@@ -116,8 +116,8 @@ type ToastProps = React.ComponentPropsWithoutRef<typeof Toast>;
 type ToastActionElement = React.ReactElement<typeof ToastAction>;
 
 export {
-  type ToastProps,
-  type ToastActionElement,
+  ToastProps,
+  ToastActionElement,
   ToastProvider,
   ToastViewport,
   Toast,
